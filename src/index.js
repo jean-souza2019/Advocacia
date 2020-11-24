cd0ef91b03e9355d7c21fef20ec77d4305c596e7
Jean Marcos <jiamarcos@outlook.com.br>
2020-11-23 23:18:35 -0300

    ADC: recado, footer

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Footer from './pages/Footer'
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/screen/ListarRecados.js
@@ -0,0 +1,69 @@
+import React, { useState, useLayoutEffect } from 'react'
+import { Grid, Paper, } from '@material-ui/core';
+// import AddCircleIcon from '@material-ui/icons/AddCircle';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Firebase from '../../services/FirebaseConnect'
+
+export default function ListarRecados(props) {
+
+    const [lista, setLista] = useState([])
+
+    useLayoutEffect(() => {
+
+
+        Firebase
+            .database()
+            .ref(`/recados`)
+            .on('value', snapchot => {
+                // converter objetos em listas
+                let dados = snapchot.val()
+                const keys = Object.keys(dados)
+                const lista = keys.map((key) => {
+                    return { ...dados[key], id: key }
+                })
+                setLista(lista)
+            })
+
+    }, [])
+
+
+    return (
+        <Grid container spacing={1} >
+            <Grid item sm={12} xs={12}>
+                <div style={{ marginTop: '10px', marginBottom: '20px', 'fontSize': '30px', 'fontFamily': 'DejaVu Sans Mono, monospace', 'color': '#3f51b5', textShadow: '0 0 1px #242c58' }}>
+                    Recados recebidos
+                </div>
+            </Grid>
+            <Grid item sm={12} xs={12}>
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow style={{ backgroundColor: '#3f51b5' }}>
+                                <TableCell style={{ color: '#fff' }}>Titulo</TableCell>
+                                <TableCell style={{ color: '#fff' }} align="right">E-mail</TableCell>
+                                <TableCell style={{ color: '#fff' }} align="center">Recado</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {lista.map((item, key) => {
+                                return <TableRow key={key}>
+                                    <TableCell component="th" scope="row">
+                                        {item.titulo}
+                                    </TableCell>
+                                    <TableCell align="right">{item.email}</TableCell>
+                                    <TableCell align="right">{item.recado}</TableCell>
+                                </TableRow>
+                            }
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
+        </Grid>
+    )
+}
